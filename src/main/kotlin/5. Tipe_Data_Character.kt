a29c971c92605e741347e8fe1c0339a22e359f07
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2022-01-26 10:58:02 +0700

    Add File Kotlin Basic

---
+++ src/main/kotlin/5. Tipe_Data_Character.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("TEST")
+    val char1 : Char = 'F'
+    val char2 : Char = 'A'
+    val char3 : Char = 'I'
+    val char4 : Char = 'S'
+    val char5 : Char = 'A'
+    val char6 : Char = 'L'
+
+    print(char1)
+    print(char2)
+    print(char3)
+    print(char4)
+    print(char5)
+    print(char6)
+}
